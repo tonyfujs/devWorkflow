e951485c8bc6fa0026a5065c9bc547f8be5d069f
Tony Fujs <tfujs@worldbank.org>
2020-09-08 11:12:21 +0200

    add basic scripts

--- R/01_gini_script.R
+++ R/01_gini_script.R
@@ -1,24 +1,30 @@
+# Below is a script you have developed to compute a gini coefficient.
+# You want to share it with your colleagues, and would like to turn this script
+# into a function.
+
+# Take some time to briefly review the code below. Then, open the
+# `02_gini_function.R` file.
+
 # Load data ---------------------------------------------------------------
 
 df <- read.csv("data-raw/microdata.csv", stringsAsFactors = FALSE)
 
-measure <- df$welfare
-weight  <- df$weight
-
 # Compute gini index ------------------------------------------------------
 
-delta_measure        <- measure * weight
+delta_measure        <- df$welfare * df$weight
 sum_weighted_measure <- sum(delta_measure, na.rm = TRUE)
 cum_measure          <- 0
 cum_weight           <- 0
 area_below_lorenz    <- 0
 
-for (i in seq_along(measure)) {
+for (i in seq_along(df$welfare)) {
 
-  area_below_lorenz <- area_below_lorenz + (cum_measure + delta_measure[i] / 2) * weight[i]
-  cum_weight        <- cum_weight + weight[i] # Cumulative weight
+  area_below_lorenz <- area_below_lorenz + (cum_measure + delta_measure[i] / 2) * df$weight[i]
+  cum_weight        <- cum_weight + df$weight[i] # Cumulative weight
   cum_measure       <- cum_measure + delta_measure[i] # Cumulative weighted measure
 
 }
 
-gini = 1 - area_below_lorenz / cum_weight / sum_weighted_measure * 2
+gini <- 1 - area_below_lorenz / cum_weight / sum_weighted_measure * 2
+
+gini
---
+++ README.md
@@ -0,0 +1,5 @@
+Welcome to the R package development workflow workshop!
+
+To get started, navigate to the `R` folder, and open the `01_gini_script.R` file.
+read along, and follow the instructions.
+
