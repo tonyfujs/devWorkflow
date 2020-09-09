ba0565ec612ac7c77466de0ec00a64d08f4481ee
Tony Fujs <tfujs@worldbank.org>
2020-09-09 10:24:53 +0200

    Add inputs validation

--- R/gini_function.R
+++ R/gini_function.R
@@ -19,6 +19,11 @@ compute_gini <- function(df,
                          welfare_col = "welfare",
                          weight_col  = "weight") {
 
+  # Validate inputs
+  check_inputs_compute_gini(df = df,
+                            welfare_col = welfare_col,
+                            weight_col = weight_col)
+
   delta_measure        <- df[[welfare_col]] * df[[weight_col]]
   sum_weighted_measure <- sum(delta_measure, na.rm = TRUE)
   cum_measure          <- 0
@@ -38,3 +43,27 @@ compute_gini <- function(df,
   return(gini)
 }
 
+
+#' check_inputs_compute_gini
+#'
+#' @param df data.frame: data.frame input to compute_gini()
+#' @param welfare_col character: Name of welfare column
+#' For instance, income.
+#' @param weight_col character: Name of weight column. I
+#'
+#' @return logical
+#'
+check_inputs_compute_gini <- function(df, welfare_col, weight_col) {
+
+  # CHECK data.frame input
+  assertthat::assert_that(is.data.frame(df),
+                          msg = glue::glue("'df' must be a data.frame vector. {df} is not a data.frame."))
+  # CHECK column inputs
+  assertthat::assert_that(is.numeric(df[[welfare_col]]),
+                          msg = glue::glue("df${welfare_col} must be a numeric vector. df${welfare_col} is not numeric."))
+  assertthat::assert_that(is.numeric(df[[weight_col]]),
+                          msg = glue::glue("df${weight_col} must be a numeric vector. df${weight_col} is not numeric."))
+  # CHECK sorting
+  assertthat::assert_that(!is.unsorted(df[[welfare_col]]),
+                          msg = glue::glue("df${welfare_col} must be sorted in increasing order"))
+}
