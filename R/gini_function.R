23ead2e66898cb98708c0f731fe12acfb9f05c97
Tony Fujs <tfujs@worldbank.org>
2020-09-09 10:01:37 +0200

    add gini function

---
+++ R/gini_function.R
@@ -0,0 +1,22 @@
+compute_gini <- function(df,
+                         welfare_col = "welfare",
+                         weight_col  = "weight") {
+
+  delta_measure        <- df[[welfare_col]] * df[[weight_col]]
+  sum_weighted_measure <- sum(delta_measure, na.rm = TRUE)
+  cum_measure          <- 0
+  cum_weight           <- 0
+  area_below_lorenz    <- 0
+
+  for (i in seq_along(df[[welfare_col]])) {
+
+    area_below_lorenz <- area_below_lorenz + (cum_measure + delta_measure[i] / 2) * df[[weight_col]][i]
+    cum_weight        <- cum_weight + df[[weight_col]][i] # Cumulative weight
+    cum_measure       <- cum_measure + delta_measure[i] # Cumulative weighted measure
+
+  }
+
+  gini <- 1 - area_below_lorenz / cum_weight / sum_weighted_measure * 2
+
+  return(gini)
+}
